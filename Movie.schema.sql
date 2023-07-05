92272bc3ff54f602f91b89b2278a2ef106979bc6
abarham97 <abodzbarham@gmail.com>
2023-07-05 08:19:44 +0000

    adding DBevar

--- Movie.schema.sql
+++ Movie.schema.sql
@@ -1,9 +1,7 @@
-CREATE TABLE movie (
+CREATE TABLE movieLIBD (
     ID serial primary key,
     Name varchar (200),
     release_date varchar(50) ,
     poster_path varchar(200),
-    overview varchar(500),
-
-
-);
+    overview varchar(500)
+)
